303a1465ccdcd173455414e8cd1a620e98583dde
Alex Beregszaszi <alex@rtfs.hu>
2016-02-16 21:04:37 +0000

    Use 'standard' for style

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
  * Bitcoin BIP32 path helpers
  * (C) 2016 Alex Beregszaszi
  */
- 
-var BIPPath = function(path) {
+
+var BIPPath = function (path) {
   this.path = path
 }
 
-BIPPath.fromTrezor = function(path) {
+BIPPath.fromTrezor = function (path) {
   return new BIPPath(path)
 }
 
-BIPPath.fromString = function(text) {
+BIPPath.fromString = function (text) {
   // skip the root
   if (text.startsWith('m/')) {
     text = text.slice(2)
@@ -21,8 +21,9 @@ BIPPath.fromString = function(text) {
   var ret = new Array(path.length)
   for (var i = 0; i < path.length; i++) {
     var tmp = /(\d+)([hH\']?)/.exec(path[i])
-    if (tmp === null)
-      throw new Error("WTF?");
+    if (tmp === null) {
+      throw new Error('Invalid input')
+    }
     ret[i] = parseInt(tmp[1], 10)
     if (tmp[2] === 'h' || tmp[2] === 'H' || tmp[2] === '\'') {
       ret[i] |= 0x80000000
@@ -31,23 +32,24 @@ BIPPath.fromString = function(text) {
   return new BIPPath(ret)
 }
 
-BIPPath.prototype.toTrezor = function() {
+BIPPath.prototype.toTrezor = function () {
   return this.path
 }
 
-BIPPath.prototype.toString = function(noRoot, oldStyle) {
+BIPPath.prototype.toString = function (noRoot, oldStyle) {
   var ret = new Array(this.path.length)
   for (var i = 0; i < this.path.length; i++) {
     var tmp = this.path[i]
-    if (tmp & 0x80000000)
+    if (tmp & 0x80000000) {
       ret[i] = (tmp & ~0x80000000) + (oldStyle ? 'h' : '\'')
-    else
+    } else {
       ret[i] = tmp
+    }
   }
   return (noRoot ? '' : 'm/') + ret.join('/')
 }
 
-BIPPath.prototype.inspect = function() {
+BIPPath.prototype.inspect = function () {
   return 'BIPPath <' + this.toString() + '>'
 }
 
